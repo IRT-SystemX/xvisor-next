bd974ab29002a084c0a5fe03419c3da90ca1593f
Julien Viard de Galbert <julien.viarddegalbert@openwide.fr>
2016-06-08 16:31:51 +0200

    fix commit adding traces to manager/thread

--- core/vmm_manager.c
+++ core/vmm_manager.c
@@ -468,7 +468,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 
 	/* Sanity checks */
 	if (name == NULL || start_pc == 0 || time_slice_nsecs == 0) {
-		vmm_lerror("%s l.%d: Failed to create vcpu %s\n",
+		vmm_lerror(NULL, "%s l.%d: Failed to create vcpu %s\n",
 			   __FUNCTION__, __LINE__, name);
 		return NULL;
 	}
@@ -500,7 +500,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 		}
 	}
 	if (!vcpu) {
-		vmm_lerror("%s l.%d: No more vcpu slot available for %s\n",
+		vmm_lerror(NULL, "%s l.%d: No more vcpu slot available for %s\n",
 			   __FUNCTION__, __LINE__, name);
 		goto fail;
 	}
@@ -511,7 +511,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 	vcpu->subid = 0;
 	if (strlcpy(vcpu->name, name, sizeof(vcpu->name)) >=
 	    sizeof(vcpu->name)) {
-		vmm_lerror("%s l.%d: Failed to set vcpu name to %s\n",
+		vmm_lerror(NULL, "%s l.%d: Failed to set vcpu name to %s\n",
 			   __FUNCTION__, __LINE__, name);
 		goto fail_avail;
 	}
@@ -536,7 +536,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 	vcpu->start_pc = start_pc;
 	vcpu->stack_va = (virtual_addr_t)vmm_malloc(stack_sz);
 	if (!vcpu->stack_va) {
-		vmm_lerror("%s l.%d: Failed to allocate vcpu %s stack\n",
+		vmm_lerror(NULL, "%s l.%d: Failed to allocate vcpu %s stack\n",
 			   __FUNCTION__, __LINE__, name);
 		goto fail_list_del;
 	}
@@ -572,7 +572,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 	/* Initialize architecture specific context */
 	vcpu->arch_priv = NULL;
 	if (arch_vcpu_init(vcpu)) {
-		vmm_lerror("%s l.%d: Failed to init vcpu %s\n",
+		vmm_lerror(NULL, "%s l.%d: Failed to init vcpu %s\n",
 			   __FUNCTION__, __LINE__, name);
 		goto fail_free_stack;
 	}
@@ -588,7 +588,7 @@ struct vmm_vcpu *vmm_manager_vcpu_orphan_create(const char *name,
 	/* Notify scheduler about new VCPU */
 	if (vmm_manager_vcpu_set_state(vcpu,
 					VMM_VCPU_STATE_RESET)) {
-		vmm_lerror("%s l.%d: Failed to set vcpu %s state\n",
+		vmm_lerror(NULL, "%s l.%d: Failed to set vcpu %s state\n",
 			   __FUNCTION__, __LINE__, name);
 		goto fail_vcpu_deinit;
 	}
